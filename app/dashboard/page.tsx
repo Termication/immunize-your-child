6a50e2c6030844f6fb4b07aabe7df31ab8fbce7d
innocent <terminalkarabo@gmail.com>
2025-10-08 23:18:48 +0200

    Add TODOs for vaccine tracking in dashboard: Outline future features for displaying received, due, and overdue vaccines for children.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,12 @@ import { useAuth } from "@clerk/clerk-expo";
 export default function Dashboard() {
   const { signOut } = useAuth();
 
+  // TODO: Add a list of vaccines that the child has received
+  // TODO: Add a list of vaccines that the child is due for
+  // TODO: Add a list of vaccines that the child is overdue for
+  // TODO: Add a list of vaccines that the child is due for next
+  // TODO: Add a list of vaccines that the child is overdue for next
+  // TODO: Add a list of vaccines that the child is due for next
   return (
     <View className="flex-1 bg-secondary dark:bg-neutral-950 px-6 pt-16">
       <Text className="text-3xl font-bold text-neutral-900 dark:text-white">Dashboard</Text>
